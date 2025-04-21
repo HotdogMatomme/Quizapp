dc07a2bbda367283e669faa57025668aea9024de
Kristian Villota <112645249+HotdogMatomme@users.noreply.github.com>
2025-04-20 17:56:56 -0800

    Add files via upload

---
+++ quizapp.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # load all the environment variable from .env file
+
+from openai import OpenAI
+OpenAI.api_key=os.getenv("OPEN_API_KEY")
+client = OpenAI()
+
+def fetch_questions(text_content, quiz_level):
+
+    RESPONSE_JSON = {
+    "mcqs" : [
+        {
+            "mcq": "multiple choice question1",
+            "options": {
+                "a": "choice here1",
+                "b": "choice here2",
+                "c": "choice here3",
+                "d": "choice here4",
+            },
+            "correct": "correct choice option",
+        },
+        {
+            "mcq": "multiple choice question1",
+            "options": {
+                "a": "choice here",
+                "b": "choice here",
+                "c": "choice here",
+                "d": "choice here",
+            },
+            "correct": "correct choice option",
+        },
+        {
+            "mcq": "multiple choice question1",
+            "options": {
+                "a": "choice here",
+                "b": "choice here",
+                "c": "choice here",
+                "d": "choice here",
+            },
+            "correct": "correct choice option",
+        }
+      ]
+    }
+
+    PROMPT_TEMPLATE="""
+    Text: {text_content}
+    You are an expert in generating MCQ type quiz on the basis of provided content.
+    Given the above text, create a quiz of 3 multiple choice questions keeping difficulty level as {quiz_level}
+    Make sure the questions are not repeated and check all the questions to be conforming the text as well.
+    Make sure to format your response like RESPONSE_JSON below and use it as a guide.
+    Ensure to make an array of 3 MCQs referring the following response json.
+    Here is the RESPONSE_JSON:
+
+    {RESPONSE_JSON}
+
+    """
+
+    formatted_template = PROMPT_TEMPLATE.format(text_content=text_content, quiz_level=quiz_level, RESPONSE_JSON=RESPONSE_JSON)
+
+    #Make API request 
+    response = client.chat.completions.create(model="chatgpt-4o-latest",
+      messages=[
+          {
+                "role": "user",
+                "content" : formatted_template                               
+          }
+      ],
+      temperature=0.3,
+      max_tokens=1000,
+      top_p=1,
+      frequency_penalty=0,
+      presence_penalty=0
+    )
+
+    # Extract response JSON
+    extracted_response = response.choices[0].message.content
+
+    print(extracted_response)
+
+    return json.loads(extracted_response).get("mcqs",[])
+
